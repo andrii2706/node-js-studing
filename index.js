2fec35ff8459a976805de3474462d2736ba9b482
andrii2706 <andriipas200017@gmail.com>
2025-10-27 21:31:32 +0200

    some practise

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const http = required('http');
+//start localhost on node.js
+// const http = require('http');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-type': 'text/plain'})
-    res.end('Hello world')
-}).listen(8080);
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-type': 'text/plain'})
+//     res.end('Hello world Andrii')
+// }).listen(8080);
+
+//show console info
+// console.log("hello from node.js"); 
+
+// creating files 
+// const fs = require('fs');
+
+// fs.writeFileSync('hello.txt', 'Hello from Node.js hello.txt');
+
