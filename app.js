c49db802441c198c6516427a5c35b235a5f26a34
andrii2706 <andriipas200017@gmail.com>
2025-11-08 15:20:15 +0200

    add some new features send get and post request

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 // one way how to start a server
 // function rqListener(req, res) {
     
@@ -31,8 +32,33 @@ const http = require('http');
 // server.listen(3000) 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers)
-    process.exit()
+    // console.log(req.url, req.method, req.headers)
+    // process.exit()
+    const url = req.url;
+    const method = req.method
+    if(url === '/'){
+    res.setHeader('Content-Type', 'text/html'); 
+    res.write('<html>');
+    res.write('<head><title>Empty Route</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Sumbit form</button></form></body>');
+    res.write('</html>');
+    return res.end();
+    }
+     if (url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.writeHead(302, {
+            'location': '/'
+        });
+       return res.end();  
+    }
+    res.setHeader('Content-Type', 'text/html'); 
+    res.write('<html>');
+    res.write('<head><title>Empty Route</title></head>');
+    res.write('<body>On Message page</body>');
+    res.write('</html>');
+    return res.end();
 })
 
-server.listen(3000) 
+
+
+server.listen(3000);
